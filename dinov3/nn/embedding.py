d978c99c37961918614fa7101f9076e013422aea
Blank (Will) <williams.edi012@gmail.com>
2026-07-14 16:14:22 -0400

    Added boilerplace for RoPE implementation

---
+++ dinov3/nn/embedding.py
@@ -0,0 +1,11 @@
+import jax
+import flax.nnx as nnx
+
+class RopeEmbedding(nnx.Module):
+    def __init__(
+        self
+    ):
+        ...
+
+    def __call__(self, *args, **kwargs):
+        return super().__call__(*args, **kwargs)
